41b07d3f1b816fb1f579b750496a4c39a7db1ad9
Mehdi Imam <mehdiimam439@gmail.com>
2023-06-10 16:54:56 -0500

    Re-organize HTML file and connect to backend

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ class UploadFileForm(FlaskForm):
     file = FileField(
         "File",
         validators=[
-            InputRequired(message="test"),
+            InputRequired(message="Input required"),
             FileAllowed(["docx", "pdf"], message="Please upload a .docx or .pdf file"),
         ],
     )
@@ -28,7 +28,9 @@ class UploadFileForm(FlaskForm):
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
-    status = "Waiting for resume..."
+    status = "Upload resume..."
+    display = "none"
+    data = None
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
@@ -43,13 +45,14 @@ def hello():
         )
         """
         # Dictionary 'data' will be parsed resume information
-        data = {"attribute": "dummy_data"}
+        data = {"attribute": "test_dummy_data"}
         db.child("Resumes").set(data)
-        status = "Resume uploaded"
+        display = "block"
     else:
         if form.file.errors:
             flash(form.file.errors[0], "error")
-    return render_template("index.html", form=form, status=status)
+    print("\nSTART\n" + str(form.file) + "\nEND\n")
+    return render_template("index.html", form=form, display=display, data=data)
 
 
 if __name__ == "__main__":
