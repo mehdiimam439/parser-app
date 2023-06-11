b22b60d835e881d1e9543af0dc4fc803a6b3b049
Mehdi Imam <mehdiimam439@gmail.com>
2023-06-11 15:18:51 -0500

    Add resume parsing and Firebase integration

--- firebase.py
+++ firebase.py
@@ -6,7 +6,7 @@ config = {
     "apiKey": "",  # "AIzaSyBsMT9l3gZTDkYUv3pCcQFHR8LeuvS4R18",
     "authDomain": "",  # "resume-parser-c757d.firebaseapp.com",
     "projectId": "",  # "resume-parser-c757d",
-    "storageBucket": "",  # "resume-parser-c757d.appspot.com",
+    "storageBucket": config.bucket,
     "messagingSenderId": "",  # "985506727421",
     "appId": "",  # "1:985506727421:web:7fe95da1b903076abbc594",
     "databaseURL": config.url,
@@ -14,3 +14,4 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+storage = firebase.storage()
--- index.py
+++ index.py
@@ -1,13 +1,21 @@
+from requests import post
 import config
 from firebase import db
+from firebase import storage
 from flask import Flask, flash, render_template
 from flask_toastr import Toastr
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from werkzeug.utils import secure_filename
-from wtforms import FileField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms import FileField, SubmitField, StringField, SearchField
+from wtforms.validators import InputRequired, DataRequired
+import uuid
+import spacy
 
+nlp = spacy.load("en_core_web_sm")
+from resume_parser import resumeparse
+
+data = None
 
 app = Flask(__name__)
 toastr = Toastr(app)
@@ -19,40 +27,74 @@ class UploadFileForm(FlaskForm):
     file = FileField(
         "File",
         validators=[
-            InputRequired(message="Input required"),
-            FileAllowed(["docx", "pdf"], message="Please upload a .docx or .pdf file"),
+            InputRequired(message="Please select a file before uploading"),
+            FileAllowed(
+                ["docx", "pdf", "txt"],
+                message="Please upload a .pdf, .docx, or .txt file",
+            ),
         ],
     )
     submit = SubmitField("Upload Resume")
 
 
+class SearchForm(FlaskForm):
+    searched = StringField(
+        "Searched", validators=[InputRequired(message="Please search something")]
+    )
+    submit = SubmitField("Submit")
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    global data
+    searched = ""
+    searchForm = SearchForm()
+    if searchForm.validate_on_submit():
+        if searchForm.searched.data:
+            searched = searchForm.searched.data
+    else:
+        if searchForm.searched.errors:
+            flash(searchForm.searched.errors[0], "warning")
+    return render_template(
+        "index.html",
+        form=searchForm,
+        searched=searched,
+        display="block",
+        data=data,
+    )
+
+
 @app.route("/", methods=["GET", "POST"])
-def hello():
-    status = "Upload resume..."
+def index():
     display = "none"
-    data = None
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
-        # Save file to ./static/files
-        """
-        file.save(
-            os.path.join(
-                os.path.abspath(os.path.dirname(__file__)),
-                app.config["UPLOAD_FOLDER"],
-                secure_filename(file.filename),
-            )
-        )
-        """
-        # Dictionary 'data' will be parsed resume information
-        data = {"attribute": "test_dummy_data"}
-        db.child("Resumes").set(data)
+        file_name = secure_filename(file.filename)
+        id = uuid.uuid4()
+        storage.child("resumes/" + file_name).put(file)
+        storage.child("resumes/" + file_name).download("resumes/", file_name)
+        global data
+        data = resumeparse.read_file(file_name)
+        for key, val in data.items():
+            if isinstance(val, list):
+                string = ""
+                for item in val:
+                    if string:
+                        string += " | " + str(item)
+                    else:
+                        string = str(item)
+                data[key] = string
+            else:
+                data[key] = str(val)
+        db.child("resumes/" + str(id)).set(data)
         display = "block"
     else:
         if form.file.errors:
             flash(form.file.errors[0], "error")
-    print("\nSTART\n" + str(form.file) + "\nEND\n")
-    return render_template("index.html", form=form, display=display, data=data)
+    return render_template(
+        "index.html", form=form, display=display, data=data, searched=""
+    )
 
 
 if __name__ == "__main__":
